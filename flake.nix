6f26fc6938e655fe40ed92c89b9e3c92de3cabb3
hugosenari <hugosenari@gmail.com>
2022-03-12 02:38:31 -0300

    chore: re index result

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Friendly, exception-free value-or-error returns, similar to Option[T]'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."result-master".url = "path:./master";
+  inputs."result-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."result-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
